76fa83c8c5f2975796534ff39813104477f80df3
Your Name <you@example.com>
2025-08-25 01:48:26 +0700

    update cv download

--- src/components/HeroPage.jsx
+++ src/components/HeroPage.jsx
@@ -1,5 +1,6 @@
 import { motion } from "motion/react";
 import Hero from "../assets/image/hero.png";
+import portoPdf from "../assets/pdf/CV.pdf";
 function HeroPage() {
   return (
     <div className="HeroPage">
@@ -19,13 +20,22 @@ function HeroPage() {
             transition={{ duration: 1.1 }}
           >
             <h2 className="text-black">Halo, Saya</h2>
-            <h1 className="text-5xl font-bold text-marine">Tegar Maulana Alfaridzi</h1>
-            <h3 className="text-black font-bold mt-10 text-xl">Junior Web Development, Web Design</h3>
+            <h1 className="text-5xl font-bold text-marine">
+              Tegar Maulana Alfaridzi
+            </h1>
+            <h3 className="text-black font-bold mt-10 text-xl">
+              Junior Web Development, Web Design
+            </h3>
             <p className=" text-black">
-              Saya seorang Junior web development dengan keahlian bahasa pemrograman PHP, HTML, CSS dan JS dan juga
-              memahami Framework Web Development
+              Saya seorang Junior web development dengan keahlian bahasa
+              pemrograman PHP, HTML, CSS dan JS dan juga memahami Framework Web
+              Development
             </p>
-            <a href="CV.pdf" download="CV.pdf" className="btn bg-marine border-none shadow-md mt-5">
+            <a
+              href={portoPdf}
+              download="CV.pdf"
+              className="btn bg-marine border-none shadow-md mt-5"
+            >
               Download CV
             </a>
           </motion.div>
