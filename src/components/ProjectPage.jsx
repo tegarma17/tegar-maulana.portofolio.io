64035a48c682e075309f7997a3ece4eb3cf342d3
Your Name <you@example.com>
2025-08-25 10:21:14 +0700

    feat:: update projectPage

--- src/components/ProjectPage.jsx
+++ src/components/ProjectPage.jsx
@@ -1,77 +1,49 @@
 import { motion } from "motion/react";
-
+import image1 from "../assets/image/aboutus.png";
+import image2 from "../assets/image/ERWE.png";
+import image3 from "../assets/image/feature.png";
+import image4 from "../assets/image/home.png";
+import image5 from "../assets/image/landingbayi.png";
+import image6 from "../assets/image/desain1.png";
+import image7 from "../assets/image/desain2.png";
 function ProjectPage() {
   return (
-    <div className="ProjectPage">
-      <section className="bg-white py-10 font-main">
-        <div className="container mx-auto">
-          <div className="project bg-white px-3 lg:pt-0 flex flex-col items-center space-y-8 ">
-            <h1 className=" md:pt-1 mt-10 lg:mt-0 text-2xl font-bold text-blue ">Project</h1>
-            <div className="filter ">
-              <input className="btn filter-reset " type="radio" name="metaframeworks" aria-label="All" />
-              <input
-                className="btn border-none bg-marine text-white"
-                type="radio"
-                name="metaframeworks"
-                aria-label="Front End"
-              />
-              <input
-                className="btn bg-marine border-none text-white"
-                type="radio"
-                name="metaframeworks"
-                aria-label="BackEnd"
-              />
-              <input
-                className="btn bg-marine border-none text-white"
-                type="radio"
-                name="metaframeworks"
-                aria-label="Full Stack"
-              />
+    <section className="bg-white py-10 font-main">
+      <div className="container mx-auto">
+        <div className="project bg-white px-3 lg:pt-0 flex flex-col items-center space-y-8 ">
+          <h1 className=" md:pt-1 mt-10 lg:mt-0 text-2xl font-bold text-blue ">
+            Project Pribadi
+          </h1>
+          <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-4 text-black">
+            <div className="">
+              <img src={image1} alt="" />
             </div>
-            <div className="box mr-5">
-              <div className="carousel carousel-center rounded-box gap-5 ">
-                <div className="carousel-item relative group ">
-                  <img src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp" alt="Burger" />
-                  <div className="absolute inset-0 flex items-center justify-center bg-black lg:bg-transparent lg:backdrop-blur-md opacity-75 lg:opacity-0 group-hover:opacity-100 transition-opacity duration-150">
-                    <div className="text-center opacity-100">
-                      <h2 className="text-white text-lg font-bold mb-2 opacity-100">Front End</h2>
-                      <button className="btn btn-primary opacity-100">Selengkapnya</button>
-                    </div>
-                  </div>
-                </div>
-
-                <div className="carousel-item relative group">
-                  <img
-                    src="https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.webp"
-                    alt="Burger"
-                    className="hover:scale-125 ease-in duration-150 w-full h-auto object-cover"
-                  />
-                  <div className="absolute inset-0 flex items-center justify-center bg-black lg:bg-transparent lg:backdrop-blur-md opacity-75 lg:opacity-0 group-hover:opacity-100 transition-opacity duration-150">
-                    <div className="text-center opacity-100">
-                      <h2 className="text-white text-lg font-bold mb-2">BackEnd</h2>
-                      <button className="btn btn-primary">Selengkapnya</button>
-                    </div>
-                  </div>
-                </div>
-                <div className="carousel-item relative group">
-                  <img
-                    src="https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.webp"
-                    alt="Burger"
-                    className="hover:scale-125 ease-in duration-150 w-full h-auto object-cover"
-                  />
-                  <div className="absolute inset-0 flex items-center justify-center bg-black lg:bg-transparent lg:backdrop-blur-md opacity-75 lg:opacity-0 group-hover:opacity-100 transition-opacity duration-150">
-                    <div className="text-center opacity-100">
-                      <h2 className="text-white text-lg font-bold mb-2">Desain Web</h2>
-                      <button className="btn btn-primary">selengkapnya</button>
-                    </div>
-                  </div>
-                </div>
+            <div>
+              <img src={image3} alt="" />
+            </div>
+            <div>
+              <img src={image4} alt="" />
+            </div>
+            <div>
+              <img src={image2} alt="" />
+            </div>
+            <div>
+              {" "}
+              <img src={image5} alt="" />
+            </div>
+            <div class="col-span-3 grid grid-cols-subgrid gap-4">
+              <div class="col-start-2">
+                {" "}
+                <img src={image6} alt="" />
+              </div>
+              <div>
+                <img src={image7} alt="" />
               </div>
             </div>
           </div>
         </div>
-      </section>
-    </div>
+      </div>
+    </section>
   );
 }
 export default ProjectPage;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -11,6 +11,7 @@ function HomePage() {
       <AboutPage />
       <AbilityPage />
       <PendidikanPage />
+      <ProjectPage />
     </>
   );
 }
